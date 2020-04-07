04e88d193aa6d7e156c30de984ffd0e12f291b9e
Ayomilotunde <51304919+Ayomilotunde@users.noreply.github.com>
2020-04-07 17:56:12 +0100

    update

---
+++ app/src/main/java/com/ayomi/toolbarpractice/DataBaseHelper.java
@@ -0,0 +1,56 @@
+package com.ayomi.toolbarpractice;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "people.db";
+    public static final String TABLE_NAME = "people_table";
+    public static final String COL1 = "ID";
+    public static final String COL2 = "NAME";
+    public static final String COL3 = "EMAIL";
+    public static final String COL4 = "DATE";
+    //public static final String COL5 = "USERNAME";
+
+    public DataBaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " + " NAME TEXT, EMAIL TEXT, DATE NUMBER, USERNAME TEXT)";
+        db.execSQL(createTable);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL(TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addData(String name, String email, String date, EditText username) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2,name);
+        contentValues.put(COL3,email);
+        contentValues.put(COL4,date);
+        //contentValues.put(COL5,username);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1) {
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+}
--- app/src/main/java/com/ayomi/toolbarpractice/MainActivity.java
+++ app/src/main/java/com/ayomi/toolbarpractice/MainActivity.java
@@ -7,14 +7,34 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    DataBaseHelper peopleDB;
+    Button btn_submit;
+    EditText new_name;
+    EditText new_date;
+    EditText new_email;
+    EditText username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        peopleDB = new DataBaseHelper(this);
+
+        new_name = findViewById(R.id.new_name);
+        new_date = findViewById(R.id.new_date);
+        new_email = findViewById(R.id.new_email);
+        username = findViewById(R.id.edt_username);
+        btn_submit = findViewById(R.id.btn_submit);
+
+        AddData();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -44,4 +64,29 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+
+    public void AddData(){
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name = new_name.getText().toString();
+                String email = new_email.getText().toString();
+                String date = new_date.getText().toString();
+                //String username = edt_username.getText().toString();
+
+                //String username;
+
+                boolean insertData = peopleDB.addData(name,email,date,username);
+
+                if (insertData == true){
+                    Toast.makeText(MainActivity.this, "Data Successfully Inserted!",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Something went Wrong!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
 }
